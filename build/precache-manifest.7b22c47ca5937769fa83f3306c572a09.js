be34169ec93740599e96e632b407b39439fe9628
Nabil Hesham <nabil1994@gmail.com>
2020-05-30 20:45:22 +0200

    second test

---
+++ build/precache-manifest.7b22c47ca5937769fa83f3306c572a09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67413e9473102813df13825bc7b9fdf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "269da43cab3d881b7b11",
+    "url": "/static/css/main.34c2ac6a.chunk.css"
+  },
+  {
+    "revision": "4dfd7f22f86b2b9a4d05",
+    "url": "/static/js/2.3bacf6a9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3bacf6a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "269da43cab3d881b7b11",
+    "url": "/static/js/main.30bda57c.chunk.js"
+  },
+  {
+    "revision": "7ebc293bff89d16a3cd9",
+    "url": "/static/js/runtime-main.ce6f6520.js"
+  }
+]);
